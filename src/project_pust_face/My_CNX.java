ceddc258390c1638bee1fdee1dbbaee8596a0706
sr computer <sr computer@DESKTOP-CC0B8RF>
2020-04-21 22:58:54 -0700

    Project_Pust_face

---
+++ src/project_pust_face/My_CNX.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_pust_face;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class My_CNX {
+    private static String servername="localhost"; 
+    private static String username="root"; 
+    private static String dbname="phone_book"; 
+    private static Integer portnumber=3306; 
+    private static String fname=""; 
+    public static Connection getConnection()
+    { 
+        Connection cnx=null; 
+        MysqlDataSource datasource=new  MysqlDataSource();
+        datasource.setServerName(servername); 
+        datasource.setUser(username);
+        datasource.setPassword(fname); 
+        datasource.setDatabaseName(dbname); 
+        datasource.setPortNumber(portnumber);
+        
+        try { 
+            cnx=datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger("Get Connection ->"+My_CNX.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        return cnx; 
+    }
+}
---
+++ src/project_pust_face/Project_Pust_face.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_pust_face;
+
+/**
+ *
+ * @author sr computer
+ */
+public class Project_Pust_face {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new main_frame().setVisible(true);
+    }
+    
+}
